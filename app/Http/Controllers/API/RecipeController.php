8b0d62a919b4c6f9507e597dd79ebc1a50cd4c77
ampmail <ampmail@ukr.net>
2018-09-29 17:15:27 +0300

    GET and DELETE methods done

--- app/Http/Controllers/API/RecipeController.php
+++ app/Http/Controllers/API/RecipeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RecipeRequest;
 use App\Recipe;
 
 class RecipeController extends Controller
@@ -15,9 +16,12 @@ class RecipeController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            return response()->json(Recipe::getUserAllRecipes());
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 422);
+        }
     }
-
 
     /**
      * Store a newly created resource in storage.
@@ -25,7 +29,7 @@ class RecipeController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(RecipeRequest $request)
     {
         //
     }
@@ -38,9 +42,15 @@ class RecipeController extends Controller
      */
     public function show($id)
     {
-        //
+        try {
+            $recipe = (array)Recipe::getUserRecipeById($id);
+            $ingredients = Recipe::getUserRecipeIngredientsById($id);
+            if ($recipe) $recipe['ingredients'] = $ingredients;
+            return response()->json($recipe);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 422);
+        }
     }
-
 
     /**
      * Update the specified resource in storage.
@@ -51,7 +61,8 @@ class RecipeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $recipe = auth()->user()->recipes()->find($id);
+        return response()->json($recipe);
     }
 
     /**
@@ -62,6 +73,14 @@ class RecipeController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try{
+            $result = Recipe::deleteUserRecipeById($id);
+            if ($result) {
+                return response()->json(['message' => 'Recipe ' . $result->name . ' has been deleted']);
+            }
+            return response()->json(['error' => 'Recipe was not found by given id'], 422);
+        } catch (\Exception $exception){
+            return response()->json(['error' => $exception->getMessage()], 422);
+        }
     }
 }
--- app/Recipe.php
+++ app/Recipe.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
@@ -10,9 +11,67 @@ class Recipe extends Model
         'name', 'text', 'user_id',
     ];
 
-
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getUserAllRecipes()
+    {
+        return DB::table('recipes')
+            ->select('recipes.id',
+                'recipes.name as recipe',
+                'recipes.text'
+            )
+            ->where('recipes.user_id', auth()->user()->id)
+            ->OrWhere('recipes.user_id', null)
+            ->orderBy('recipes.id')
+            ->get();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public static function getUserRecipeIngredientsById($id)
+    {
+        return DB::table('recipe_ingredient')
+            ->select('recipe_ingredient.ingredient_id',
+                'ingredients.name',
+                'recipe_ingredient.amount')
+            ->join('ingredients', 'recipe_ingredient.ingredient_id', '=', 'ingredients.id')
+            ->where('recipe_ingredient.recipe_id', $id)
+            ->get();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public static function getUserRecipeById($id)
+    {
+        return DB::table('recipes')
+            ->select('recipes.id',
+                'recipes.name as recipe',
+                'recipes.text')
+            ->where([['recipes.user_id', auth()->user()->id], ['recipes.id', $id]])
+            ->oRwhere([['recipes.user_id', null], ['recipes.id', $id]])
+            ->first();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public static function deleteUserRecipeById($id)
+    {
+        $recipe = auth()->user()->recipes()->find($id);
+        if ($recipe) {
+            $recipe->delete();
+        }
+        return $recipe;
     }
 }

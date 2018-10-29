3e275d13ba50080d9cef42040bc67bf6ae41f8b7
Vitalii Ivanov <vitalii.ivanov1983@gmail.com>
2018-10-29 21:21:57 +0200

    panel active

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,21 @@
         <div class="row">
             <div class="col-md-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading"><a href="{{route('dashboard.rules')}}">Rules</a></div>
-                    <div class="panel-heading"><a href="{{route('dashboard.user.index')}}">Users</a></div>
-                    <div class="panel-heading"><a href="{{route('dashboard.ingredients.index')}}">Ingredients</a></div>
-                    <div class="panel-heading"><a href="{{route('dashboard.recipes.index')}}">Recipes</a></div>
-                    <div class="panel-heading"><a href="{{route('dashboard.analytics')}}">Analytics</a></div>
+                    <div class="panel-heading"><a
+                                style="font-weight: {{ request()->route()->getName() === 'dashboard.rules' ? 'bold' : '' }}"
+                                href="{{route('dashboard.rules')}}">Rules</a></div>
+                    <div class="panel-heading"><a
+                                style="font-weight: {{ request()->route()->getName() === 'dashboard.user.index' ? 'bold' : '' }}"
+                                href="{{route('dashboard.user.index')}}">Users</a></div>
+                    <div class="panel-heading"><a
+                                style="font-weight: {{ request()->route()->getName() === 'dashboard.ingredients.index' ? 'bold' : '' }}"
+                                href="{{route('dashboard.ingredients.index')}}">Ingredients</a></div>
+                    <div class="panel-heading"><a
+                                style="font-weight: {{ request()->route()->getName() === 'dashboard.recipes.index' ? 'bold' : '' }}"
+                                href="{{route('dashboard.recipes.index')}}">Recipes</a></div>
+                    <div class="panel-heading"><a
+                                style="font-weight: {{ request()->route()->getName() === 'dashboard.analytics' ? 'bold' : '' }}"
+                                href="{{route('dashboard.analytics')}}">Analytics</a></div>
 
                 </div>
             </div>
@@ -18,7 +28,7 @@
             </div>
 
             <div class="col-md-2">
-                <div class="card" >
+                <div class="card">
                     <div class="card-header">
                         <p>EVENTS</p>
                     </div>
@@ -28,4 +38,5 @@
             </div>
         </div>
     </div>
+
 @endsection
